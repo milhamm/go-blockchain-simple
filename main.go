7e007ff5961a8508967314d5dee54fea72c52107
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-10-22 21:41:14 +0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+type Block struct{
+	Index		int
+	Timestamp	string
+	BPM			int
+	Hash		string
+	PrevHash	string
+}
+
+var BlockChain []Block
+
+func createHash(block Block) string{
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
